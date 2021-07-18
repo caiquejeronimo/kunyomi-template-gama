6e9c3c26938987b07bddf3c05e6d8d68ce480b2c
Caique Jeronimo <caiquejeronimo@outlook.com>
2021-07-18 15:05:49 -0300

    Adicionado local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,48 @@
 function alerta_newsletter() {
     alert("Email cadastrado!");
 }
+$(function () {
+    var operacao = "A"; //"A"=Adição; "E"=Edição
+    var indice_selecionado = -1; //Índice do item selecionado na lista
+    var tabelaClientes = localStorage.getItem("tabelaClientes"); // Recupera os dados armazenados
+    tabelaClientes = JSON.parse(tabelaClientes); // Converte string para objeto
+    if (tabelaClientes == null) // Caso não haja conteúdo, iniciamos um vetor vazio
+        tabelaClientes = [];
+});
+
+function Adicionar() {
+    var cliente = JSON.stringify({
+        Email: $("#emailnewsletter").val()
+    });
+    tabelaClientes.push(cliente);
+    localStorage.setItem("tabelaClientes", JSON.stringify(tabelaClientes));
+    alert("Registro adicionado.");
+    return true;
+}
+
+function Editar() {
+    tabelaClientes[indice_selecionado] = JSON.stringify({
+
+        Email: $("#emailnewsletter").val()
+    }); //Altera o item selecionado na tabela
+    localStorage.setItem("tabelaClientes", JSON.stringify(tabelaClientes));
+    alert("Informações editadas.")
+    operacao = "A"; //Volta ao padrão
+    return true;
+}
+
+function Excluir() {
+    tabelaClientes.splice(indice_selecionado, 1);
+    localStorage.setItem("tabelaClientes", JSON.stringify(tabelaClientes));
+    alert("Registro excluído.");
+}
+
+function Listar() {
+    $("#tblLiWistar tbody").append("</tr>");
+}
+$("#subscribe_popup").on("submit", function () {
+    if (operacao == "A")
+        return Adicionar();
+    else
+        return Editar();
+});
